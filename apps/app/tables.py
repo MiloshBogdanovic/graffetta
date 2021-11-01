7f7ae3e640bec4541d2f5c10d3c410804dcffad1
Kyle O'Callaghan <k.oconnor@westradeltd.com>
2021-11-01 13:41:41 +0100

    Added translations for table headers

--- apps/app/tables.py
+++ apps/app/tables.py
@@ -8,8 +8,7 @@ class CondominiumTable(tables.Table):
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.name,
-                'data-type':'name',
-                'data-table':'condo'
+                'data-type':'name'
         }
     })
 
@@ -17,8 +16,7 @@ class CondominiumTable(tables.Table):
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.fiscal_code,
-                'data-type':'fiscal_code',
-                'data-table':'condo'
+                'data-type':'fiscal_code'
         }
     })
 
@@ -177,28 +175,28 @@ class AdministrationLegalTable(tables.Table):
                 'data-id': lambda record: record.company_name,
                 'data-type':'company_name'
         }
-    })
+    }, verbose_name='Nome commerciale')
     province = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.province,
                 'data-type':'province'
         }
-    })
+    }, verbose_name='Provincia')
     vat_number = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.vat_number,
                 'data-type':'vat_number'
         }
-    })
+    }, verbose_name='Partita IVA')
     street = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.street,
                 'data-type':'street'
         }
-    })
+    }, verbose_name='strada')
     cap = tables.Column(attrs={
         'td': {
                 'class': 'editable',
@@ -212,49 +210,49 @@ class AdministrationLegalTable(tables.Table):
                 'data-id': lambda record: record.municipal_reg_office,
                 'data-type':'municipal_reg_office'
         }
-    })
+    }, verbose_name='sede legale comunale')
     province_reg_office = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.province_reg_office,
                 'data-type':'province_reg_office'
         }
-    })
+    }, verbose_name='sede legale della provincia')
     legal_title_rep = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.legal_title_rep,
                 'data-type':'legal_title_rep'
         }
-    })
+    }, verbose_name='titolo rappresentativo ')
     leg_rep_name = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.leg_rep_name,
                 'data-type':'leg_rep_name'
         }
-    })
+    }, verbose_name='nome del rappresentante ')
     leg_rep_tax_code = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.leg_rep_tax_code,
                 'data-type':'leg_rep_tax_code'
         }
-    })
+    }, verbose_name='codice fiscale rappresentativo ')
     leg_rep_dob = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.leg_rep_dob,
                 'data-type':'leg_rep_dob'
         }
-    })
+    }, verbose_name='data di nascita rappresentativa ')
     municipal_of_birth_of_leg = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.municipal_of_birth_of_leg,
                 'data-type':'municipal_of_birth_of_leg'
         }
-    })
+    }, verbose_name='data di nascita rappresentativa comune ')
     province_of_birth_of_leg = tables.Column(attrs={
         'td': {
                 'class': 'editable',
@@ -268,7 +266,7 @@ class AdministrationLegalTable(tables.Table):
                 'data-id': lambda record: record.legal_street,
                 'data-type':'legal_street'
         }
-    })
+    }, verbose_name='strada legale')
 
     cap_legal = tables.Column(attrs={
         'td': {
@@ -276,7 +274,7 @@ class AdministrationLegalTable(tables.Table):
                 'data-id': lambda record: record.cap_legal,
                 'data-type':'cap_legal'
         }
-    })
+    }, verbose_name='cap legale')
 
     municipal_of_leg_residence = tables.Column(attrs={
         'td': {
@@ -284,7 +282,7 @@ class AdministrationLegalTable(tables.Table):
                 'data-id': lambda record: record.municipal_of_leg_residence,
                 'data-type':'municipal_of_leg_residence'
         }
-    })
+    }, verbose_name='comune di residenza legale')
 
     province_of_leg_residence = tables.Column(attrs={
         'td': {
@@ -292,13 +290,13 @@ class AdministrationLegalTable(tables.Table):
                 'data-id': lambda record: record.province_of_leg_residence,
                 'data-type':'province_of_leg_residence'
         }
-    })
+    }, verbose_name='provincia di residenza legale ')
 
     class Meta:
         model = AdministrationLegal
         row_attrs = {
             "data-id": lambda record: record.pk,
-            "data-table":'catastal'
+            "data-table":'legal'
         }
         attrs = {
             'class': 'table table-hover m-b-0'
@@ -311,104 +309,104 @@ class AdministrationIndividualTable(tables.Table):
                 'data-id': lambda record: record.title,
                 'data-type':'title'
         }
-    })
+    }, verbose_name='titolo')
     name = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.name,
                 'data-type':'name'
         }
-    })
+    }, verbose_name='nome e cognome')
     fiscal_code = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.fiscal_code,
                 'data-type':'fiscal_code'
         }
-    })
+    }, verbose_name='codice fiscale')
     vat_number = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.vat_number,
                 'data-type':'vat_number'
         }
-    })
+    }, verbose_name='Partita IVA')
     dob = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.dob,
                 'data-type':'dob'
         }
-    })
+    }, verbose_name='data di nascita')
     birthplace = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.birthplace,
                 'data-type':'birthplace'
         }
-    })
+    }, verbose_name='luogo di nascita')
     birthplace_county = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.birthplace_county,
                 'data-type':'birthplace_county'
         }
-    })
+    }, verbose_name='contea natale')
     activity_street = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.activity_street,
                 'data-type':'activity_street'
         }
-    })
+    }, verbose_name='via delle attività')
     activity_location_cap = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.activity_location_cap,
                 'data-type':'activity_location_cap'
         }
-    })
+    }, verbose_name='via delle attività')
     activity_municipality = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.activity_municipality,
                 'data-type':'activity_municipality'
         }
-    })
+    }, verbose_name='comune di attività')
     activity_province = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.activity_province,
                 'data-type':'activity_province'
         }
-    })
+    }, verbose_name='provincia di attività')
     residence_street = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.residence_street,
                 'data-type':'residence_street'
         }
-    })
+    }, verbose_name='via della residenza')
     residence_city = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.residence_city,
                 'data-type':'residence_city'
         }
-    })
+    }, verbose_name='città di residenza')
     residence_province = tables.Column(attrs={
         'td': {
                 'class': 'editable',
                 'data-id': lambda record: record.residence_province,
                 'data-type':'residence_province'
         }
-    })
+    }, verbose_name='provincia di residenza')
 
     class Meta: 
         model = AdministrationIndividual
         row_attrs = {
             "data-id": lambda record: record.pk,
-            "data-table":'catastal'
+            "data-table":'individual'
         }
         attrs = {
             'class': 'table table-hover m-b-0'
