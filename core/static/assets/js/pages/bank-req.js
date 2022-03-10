246de45d7dcd2051217c2da9e8d78c3fd7c3c38d
MiloshBogdanovic <m.bogdanovic@westradeltd.com>
2022-03-10 15:13:38 +0100

    js bug fixed

--- core/static/assets/js/pages/bank-req.js
+++ core/static/assets/js/pages/bank-req.js
@@ -1,16 +1,15 @@
-
-$('#pills-tab a').click(function(e) {
-  e.preventDefault();
-  $(this).tab('show');
-});
-
-// store the currently selected tab in the hash value
-$("ul.nav-pills > li > a").on("shown.bs.tab", function(e) {
-  var id = $(e.target).attr("href").substr(1);
-  print(id)
-  window.location.hash = id;
-});
-
-// on load of the page: switch to the currently selected tab
-var hash = window.location.hash;
-console.log(hash)
+//
+//$('#pills-tab a').click(function(e) {
+//  e.preventDefault();
+//  $(this).tab('show');
+//});
+//
+//// store the currently selected tab in the hash value
+//$("ul.nav-pills > li > a").on("shown.bs.tab", function(e) {
+//  var id = $(e.target).attr("href").substr(1);
+//    console.log(id)
+//  window.location.hash = id;
+//});
+//
+//// on load of the page: switch to the currently selected tab
+//var hash = window.location.hash;
